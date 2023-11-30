5e2bdfb7f93315e48e3075c94028c69ca17818df
deril1709 <punktator17@gmail.com>
2023-12-01 02:16:52 +0800

    bzz

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ const {Client, Collection, GatewayIntentBits} = require('discord.js');
 const fs = require('fs');
 
 
-const client = client({ intents : GatewayIntentBits.Guilds   });
-client.commands = new Collection;
+const client = new Client({ intents : GatewayIntentBits.Guilds   });
+client.commands = new Collection();
 client.commandArray = [];
 
 const functionFolders = fs.readdirSync('./src/function')
 for (const folder of functionFolders){
     const functionFiles = fs
-    .readdirSync(`./src/function${folder}`)
+    .readdirSync(`./src/function/${folder}`)
     .filter((file) => file.endsWith('.js'));
-    for (const file of functionFiles) require(`./function/${folder}/${file}`)(Client);
+    for (const file of functionFiles)
+    require(`./function/${folder}/${file}`)(client);
 }
+
+client.handleEvents();
+client.handleCommands();
+client.login(token);
